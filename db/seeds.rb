11cf88a5e67c74c98ce8664e0b6c9e7ec0da4a7e
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-09-27 18:02:55 +0100

    Add some restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+restaurants = [
+  {
+    name: "Nando's Goodge Street",
+    opening_hours: "11am - 11pm Daily",
+    address: "Goodge Street",
+  },
+  {
+    name: "Yoobi",
+    opening_hours: "11am - 11pm Daily",
+    address: "38 Lexington Street",
+  },
+  {
+    name: "Pod TCR",
+    opening_hours: "8am - 11pm Daily",
+    address: "Tottenham Court Road",
+  },
+  {
+    name: "Meat Liquor",
+    opening_hours: "11am - 11pm Daily",
+    address: "Covent Garden",
+  },
+]
+
+restaurants.each do |resto_attrs|
+  Restaurant.create!(resto_attrs)
+end
